bef8e4a67720768691439eec26d5c2096b0830b5
olliswe <oliveriyer@gmail.com>
2020-01-21 21:39:23 +0000

    changed page title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -64,20 +64,20 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
+                {/*<a title="facebook" href="https://facebook.com">*/}
+                {/*  <img*/}
+                {/*    src={facebook}*/}
+                {/*    alt="Facebook"*/}
+                {/*    style={{ width: '1em', height: '1em' }}*/}
+                {/*  />*/}
+                {/*</a>*/}
+                {/*<a title="instagram" href="https://instagram.com">*/}
+                {/*  <img*/}
+                {/*    src={instagram}*/}
+                {/*    alt="Instagram"*/}
+                {/*    style={{ width: '1em', height: '1em' }}*/}
+                {/*  />*/}
+                {/*</a>*/}
               </div>
             </div>
           </div>
